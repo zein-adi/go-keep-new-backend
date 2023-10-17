74f691f6482f3196a2a61c4a2b5d0d64d111b7ec
Zein Adi <zain.adiyusuf@gmail.com>
2023-10-17 18:54:25 +0700

    menambahkan fungsi create pada migration

--- migration.go
+++ migration.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"github.com/golang-migrate/migrate"
 	"github.com/golang-migrate/migrate/database/mysql"
@@ -9,42 +8,31 @@ import (
 	"github.com/zein-adi/go-keep-new-backend/helpers/helpers_error"
 	"github.com/zein-adi/go-keep-new-backend/helpers/helpers_mysql"
 	"github.com/zein-adi/go-keep-new-backend/helpers/validator"
+	"os/exec"
 	"strconv"
 )
 
-func RunMigration() {
-	domainList := map[string]string{
-		"auth": "domains/auth/repos/auth_repos_mysql/migrations",
-	}
-
-	args := flag.Args()
-	if len(args) < 3 {
-		panic("usage: migrate [action: up|down|version|force] [domain] [options]")
-	}
-	action := flag.Arg(1)
-	domain := flag.Arg(2)
-	version := flag.Arg(3)
-
+func RunMigration(action, domain, version, name string) {
 	// Validation
 	v := validator.New()
 	data := map[string]interface{}{
 		"action":  action,
 		"domain":  domain,
 		"version": version,
+		"name":    name,
 	}
 	rules := map[string]interface{}{
-		"action":  "required,oneof=up down version force",
-		"domain":  "required,oneof=auth",
+		"action":  "required,oneof=up down version force create",
 		"version": "required_if=action force,min=0",
+		"domain":  "required_if=action create",
+		"name":    "required_if=action create",
 	}
 	err := v.ValidateMap(data, rules)
 	helpers_error.PanicIfError(err)
 
 	// Initialization
-	m := &Migration{
-		domainList: domainList,
-	}
-	m.init(domain)
+	m := &Migration{}
+	m.init()
 
 	// Execution
 	switch action {
@@ -63,12 +51,18 @@ func RunMigration() {
 		versionInt, err := strconv.Atoi(version)
 		helpers_error.PanicIfError(err)
 		m.Force(versionInt)
+	case "create":
+		fmt.Printf("Creating Migration: %s %s \n", domain, name)
+		resultString, err := m.Create(domain, name)
+		if err != nil {
+			fmt.Print("error: ")
+		}
+		fmt.Println(resultString)
 	}
 }
 
 type Migration struct {
 	*migrate.Migrate
-	domainList map[string]string
 }
 
 func (m *Migration) Up() {
@@ -110,10 +104,15 @@ func (m *Migration) Close() {
 	helpers_error.PanicIfError(se)
 	helpers_error.PanicIfError(de)
 }
-func (m *Migration) init(domain string) {
+func (m *Migration) init() {
 	db, _ := helpers_mysql.OpenMySqlConnection()
 	driver, err := mysql.WithInstance(db, &mysql.Config{})
 	helpers_error.PanicIfError(err)
-	m.Migrate, err = migrate.NewWithDatabaseInstance("file://"+m.domainList[domain], "mysql", driver)
+	m.Migrate, err = migrate.NewWithDatabaseInstance("file://migrations", "mysql", driver)
 	helpers_error.PanicIfError(err)
+}
+func (m *Migration) Create(domain, name string) (string, error) {
+	cmd := exec.Command("migrate", "create", "-ext", "sql", "-dir", "migrations", fmt.Sprintf("%s__%s", domain, name))
+	res, err := cmd.CombinedOutput()
+	return string(res), err
 }
